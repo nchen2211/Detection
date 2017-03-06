09b65c42fb01bc6b66a4d78fcb120e708e2e71cd
nchen2211 <nchrysilla@gmail.com>
2017-03-06 02:25:09 -0800

    bluetooth alert (sunday fix_

--- BluetoothAlert/app/src/main/java/monitor/sudep/application/bluetoothalert/MainActivity.java
+++ BluetoothAlert/app/src/main/java/monitor/sudep/application/bluetoothalert/MainActivity.java
@@ -19,19 +19,31 @@ public class MainActivity extends AppCompatActivity {
     // Return Intent extra
     public static String EXTRA_DEVICE_ADDRESS = "device_address";
     private String mSpecificDevice = "Nexus 7";
+    private boolean isFound;
 
 
     // Member fields
-    private BluetoothAdapter mBtAdapter;
+   // private BluetoothAdapter mBtAdapter;
+    private BluetoothAdapter adapter;
+    BluetoothDevice device;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+        adapter = BluetoothAdapter.getDefaultAdapter();
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(BluetoothDevice.ACTION_FOUND);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+
+        registerReceiver(mReceiver, filter);
+        adapter.startDiscovery();
 
         // Set result CANCELED in case the user backs out
-        setResult(Activity.RESULT_CANCELED);
+        /* setResult(Activity.RESULT_CANCELED);
 
         // Register for broadcasts when a device is discovered
         IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
@@ -43,61 +55,114 @@ public class MainActivity extends AppCompatActivity {
 
         // Get the local Bluetooth adapter
         mBtAdapter = BluetoothAdapter.getDefaultAdapter();
+        mBtAdapter.startDiscovery();*/
     }
-
-
-    public void doDiscovery(View view) {
-        doDiscovery();
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        // Make sure we're not doing discovery anymore
-        if (mBtAdapter != null) {
-            mBtAdapter.cancelDiscovery();
-        }
-
-        // Unregister broadcast listeners
-        this.unregisterReceiver(mReceiver);
-    }
-
-    /**
-     * Start device discover with the BluetoothAdapter
-     */
-    private void doDiscovery() {
-
-        Log.d(TAG, "##################### in doDiscovery");
-        // If we're already discovering, stop it
-        if (mBtAdapter.isDiscovering()) {
-            mBtAdapter.cancelDiscovery();
-        }
-
-        // Request discover from BluetoothAdapter
-        mBtAdapter.startDiscovery();
-    }
-
 
     private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
-        @Override
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
 
-            // When discovery finds a device
-            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                // Get the BluetoothDevice object from the Intent
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                Log.d(TAG, "device name: " + device.getName());
-                if (mSpecificDevice.equals(device.getName())) {
-                    Toast.makeText(getApplicationContext(), device.getName() + " is on", Toast.LENGTH_LONG).show();
+            Log.d(TAG, "in broadcast receiver");
+            if (BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action)) {
+                Log.d(TAG, "start..");
+                //discovery starts, we can show progress dialog or perform other tasks
+            } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+                Log.d(TAG, "finish..");
+
+                if (device.getName() == null) {
+                    //if (! device.getName().equals(mSpecificDevice)) {
+                    Log.d(TAG, "discovering again.. ");
+                    adapter.startDiscovery();
+                    Log.d(TAG, "device wrong name: " + device.getName());
                 }
+//
+                //discovery finishes, dismis progress dialog
+            } else if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                Log.d(TAG, "found..");
+                //bluetooth device found
+                device = (BluetoothDevice) intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                Log.d (TAG, "device name: " + device.getName());
+
+                if (device.getName() == null) {
+                    //if (! device.getName().equals(mSpecificDevice)) {
+                    Log.d(TAG, "discovering again.. ");
+                    adapter.startDiscovery();
+                    Log.d(TAG, "device wrong name: " + device.getName());
+                }
+//
+                Log.d (TAG, "device name: " + device.getName());
+                if (mSpecificDevice.equals(device.getName())) {
+                    Log.d (TAG, "cancel discovery");
+                    adapter.cancelDiscovery();
+                }
+
+                Toast.makeText(getApplicationContext(), device.getName() + " is on", Toast.LENGTH_LONG).show();
             }
         }
     };
+//
+//    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+//        @Override
+//        public void onReceive(Context context, Intent intent) {
+//            String action = intent.getAction();
+//            Log.d(TAG, "in BroadcastReceiver");
+//
+//            // When discovery finds a device
+//            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+//                // Get the BluetoothDevice object from the Intent
+//                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+//
+//                while (! isFound) {
+//                    Log.d(TAG, "search again....");
+//                    mBtAdapter.startDiscovery();
+//
+//                    // If we're already discovering, stop it
+//                    if (mBtAdapter.isDiscovering()) {
+//                        Log.d(TAG, "FOUND....");
+//                        mBtAdapter.cancelDiscovery();
+//                        //isFound = true;
+//                    }
+//                }
+//
+//                Log.d(TAG, "device name: " + device.getName());
+//
+//                if (mSpecificDevice.equals(device.getName())) {
+//                    Toast.makeText(getApplicationContext(), device.getName() + " is on", Toast.LENGTH_LONG).show();
+//                }
+//            }
+//        }
+//    };
+
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//    }
+//
+//    @Override
+//    protected void onDestroy() {
+//        super.onDestroy();
+//
+//        // Make sure we're not doing discovery anymore
+//        if (mBtAdapter != null) {
+//            mBtAdapter.cancelDiscovery();
+//        }
+//
+//        // Unregister broadcast listeners
+//        this.unregisterReceiver(mReceiver);
+//    }
+//
+//    /**
+//     * Start device discover with the BluetoothAdapter
+//     */
+//    private void doDiscovery() {
+//
+//        Log.d(TAG, "##################### in doDiscovery");
+//        // If we're already discovering, stop it
+//        if (mBtAdapter.isDiscovering()) {
+//            mBtAdapter.cancelDiscovery();
+//        }
+//
+//        // Request discover from BluetoothAdapter
+//        mBtAdapter.startDiscovery();
+//    }
 }
